6ba67431bb9d73df343eab3339198d28b093d81a
Ryan Hu <ryanhuuu@gmail.com>
2018-04-24 15:39:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
   get '/' do
@@ -8,6 +9,29 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    @words = text_from_user.split(' ').length
+    split = text_from_user.gsub(/\s+/,'').split('')
+    @vowels = split.select do |el|
+      ["a", "e", "i", "o", "u"].include?(el.downcase)
+    end.length
+    @consonants = split.length - @vowels
+    @most = ''
+    @most_count = 0
+    hash = {}
+    split.each do |el|
+      if hash[el] != nil
+        hash[el] += 1
+        if hash[el] >= @most_count
+          @most_count = hash[el]
+          @most = el
+        end
+      else
+        hash[el] = 1
+      end
+    end
+
+    @most = @most.upcase
+
 
     erb :results
   end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,26 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  attr_accessor :text
+  def initialize(string)
+    @text = string
+  end
+
+  def count_of_words
+
+  end
+
+  def count_of_vowels
+
+  end
+
+  def count_of_consonants
+
+  end
+
+  def most_used_letter
+
+  end
+
+end
+
+my_text = TextAnalyzer.new("The rain in Spain stays mainly on the plain.")
